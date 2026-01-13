6a642433bca7d5db413324de229a7a1c17c80b82
Reiss Bhadhal <reiss@bhadhal.com>
2026-01-13 17:48:54 +0000

    Update Game: Tue Jan 13 17:48:54 GMT 2026

--- publish_game.sh
+++ publish_game.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
 # Script to deploy Space Invaders to GitHub
 
 echo "🚀 Preparing Deployment..."
 cd /Users/reissbhadhal/projects/space-invaders
-
-# Copy latest files to deploy folder (Just in case)
-cp index.html deploy/reiss/index.html
-cp game.js deploy/reiss/game.js
-cp style.css deploy/reiss/style.css
-cp SocialManager.js deploy/reiss/SocialManager.js
-cp NetworkManager.js deploy/reiss/NetworkManager.js
 
 # Git Operations
 echo "📦 Committing changes..."
@@ -24,4 +16,4 @@ echo "If you have 2FA enabled, you must use a Personal Access Token as the passw
 git remote set-url origin https://github.com/reissbhadhal/bhadhal.com.git
 git push origin main
 
-echo "✅ Done! Check https://bhadhal.com/reiss/"
+echo "✅ Done! Check https://bhadhal.com/"
